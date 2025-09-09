8500181ae4d9cc393931fbc4fbe9bce652f50576
CrisLP <cristianlupasco2004@gmail.com>
2025-09-09 15:25:39 +0300

    Header and Footer

--- frontend/app/(tabs)/about.tsx
+++ frontend/app/(tabs)/about.tsx
@@ -1,22 +1,34 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Header from "@/components/HomeHeader";
+import { Alert } from "react-native";
+
+const handleSearchPress = () => {
+  Alert.alert("Search", "Search functionality coming soon!");
+};
+
+const handleNotificationPress = () => {
+  Alert.alert("Notifications", "Notification functionality coming soon!");
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        color: '#fff',
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#fff",
+  },
+});
 
 export default function AboutScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>About screen</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Header
+        userName="Mr. Williamson"
+        onSearchPress={handleSearchPress}
+        onNotificationPress={handleNotificationPress}
+      />
+    </View>
+  );
 }
